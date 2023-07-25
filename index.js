db6d140fd0f85321aeb2749e7c67175123b829e3
Ahkar Hein <ahkarhein04@gmail.com>
2023-07-25 09:11:16 -0700

    create a array and put a questions into array

--- index.js
+++ index.js
@@ -3,13 +3,63 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your github username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'What is your project name?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a description for your project',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your application:',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'What commend should be run to install dependecies?',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What commend should be run to run test?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What dose the user need to know about using the repo?',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What does user need to know about the contributing to the repo',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
